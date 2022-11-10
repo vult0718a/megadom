11b841fd2818acb13c7d65a5c19981c65f013a6d
Pham Trung Kien <kiencampha@gmail.com>
2022-11-10 12:25:49 +0700

    2-5 den 2-8

---
+++ resources/views/user/page/2-5.blade.php
@@ -0,0 +1,127 @@
+@extends('user.layout.main')
+
+@section('title')
+<title>Làm to viền quy đầu</title>
+@endsection
+
+@section('style')
+<link rel="stylesheet" type="text/css" href="/user/page/2-5.css">
+@endsection
+
+@section('content')
+<div class="screen2-5">
+    <div class="banner">
+            <p class="text-medium">TRANG CHỦ. PHẪU THUẬT NAM KHOA. PHẪU THUẬT QUY ĐẦU</p>
+            <p class="text-bold">Phẫu thuật </br>làm to viền quy đầu</p>
+    </div> 
+    <div class="ss1">
+        <p class="text-1">PHẪU THUẬT LÀM TO VIỀN QUY ĐẦU</p>
+        <span class="line"></span>
+        <p class="text-2">Phẫu thuật làm to viền quy đầu là một phương pháp đã được áp dụng từ rất lâu trên Thế giới.
+            Phương pháp này làm dày viền quy đầu lên bằng cách sử dụng chất chủ đạo Hyaluronic acid,
+            hay còn được gọi là Filler. </p>
+        <div class="ss1-1">
+            <div class="text-left">
+                <p class="text-1">HYALURONIC ACID / FILLER</p>
+                <span class="line"></span>
+                <p class="text-2">Bản chất Hyaluronic acid là một chất làm đầy mô, tổ chức dưới da của cơ thể. 
+                    Trong phẫu thuật thẩm mỹ Hyaluronic acid thường được sử dụng trong phẫu thuật độn cằm, nâng
+                    mũi, bơm môi. Filler tiêm vào viền quy đầu là những hạt ngậm nước, có cấu trúc tương tự như
+                    Hyaluronic acid trong cơ thể người.</p>
+            </div>
+            <div class="text-right"></div>
+        </div>
+    </div>
+    <div class="ss2">
+        <p class="text-1">ĐẶT LỊCH TƯ VẤN MIỄN PHÍ</p>
+        <button class="btn-tu-van">
+            <p class="text-1">ĐĂNG KÝ NGAY</p>
+        </button>
+    </div>
+    <div class="ss3">
+        <div class="text-left"></div>
+        <div class="text-right">
+            <p class="text-1">HIỆU QUẢ</p>
+            <span class="line"></span>
+            <p class="text-2">Thông thường, tiêm Filler vào viền quy đầu có thể duy trì được 12 - 18 tháng. 
+                Sau đó Filler sẽ tự tiêu và đào thải khỏi cơ thể. Bản chất Filler là chất làm đầy mô, là chất 
+                ngoại lai nên nó không tồn tại vĩnh viễn trong cơ thể.</br></br>
+                Nếu bạn lựa chọn chỉ làm to viền quy đầu thì sau khoảng hơn 1 năm thì có thể tiêm lại. Nếu kết
+                 hợp giữa làm to quy đầu và làm to viền quy đầu thì việc tiêm lại Filler là không cần thiết. 
+                 Lý do là bởi việc phẫu thuật làm to quy đầu cũng giúp phần viền quy đầu tăng lên đáng kể. </p>
+        </div>
+    </div>
+    <div class="ss4">
+        <div class="img">
+            <div class="text-left">
+                <p class="text-1">LỰA CHỌN PHÒNG KHÁM NAM KHOA MEGADOM</p>
+            </div>
+            <div class="text-right">
+                <p class="text-2">Trải nghiệm của bệnh nhân rất quan trọng đối với Megadom.
+                    Chúng tôi đối xử với từng bệnh nhân bằng sự chăm sóc tận tình, lòng trắc ẩn và sự quan tâm sâu sắc.
+                    Các chuyên gia của Megadom luôn đồng hành cùng bạn trong từng bước, từ buổi tư vấn ban đầu, 
+                    cho đến khi điều trị, và hơn thế nữa là cam kết hỗ trợ trọn đời. Chúng tôi tự hào về  những thành công 
+                    đã đạt được và sự hài lòng cao của bệnh nhân tại Megadom. Với vô số các ca phẫu thuật đã được thực hiện
+                    cho đến nay, bạn có thể tin rằng mình đang được chăm sóc bởi một trong những chuyên gia hàng đầu trong 
+                    lĩnh vực này.</p>
+            </div>
+        </div>
+    </div>
+    <div class="ss5">
+        <div class="text-left">
+            <p class="text-1">ƯU ĐIỂM CỦA PHƯƠNG PHÁP LÀM TO VIỀN QUY ĐẦU BẰNG FILLER</p>
+            <span class="line"></span>
+            <p class="text-2">Tiêm Filler vào viền quy đầu được đánh giá là một phương pháp tốt nhất hiện nay để làm to
+                 phần viền của quy đầu: </br></br></br>
+                
+                 &#8226; Không gây kích ứng da,</br>
+                 &#8226; Hiệu quả trong thời gian dài, từ 12 - 18 tháng </br>
+                 &#8226; Tính thẩm mỹ cao</br></br></br>
+
+                Khi tiêm Filler vào viền quy đầu thì chỉ với một lượng nhỏ Filler, phần viền quy đầu sẽ to lên,
+                 các bác sĩ sẽ điều chỉnh để cho phần viền này to đều, căng và nhìn rất mịn.</p>
+        </div>
+        <div class="text-right"></div>
+    </div>
+    <div class="ss6">
+        <div class="text-left"></div>
+        <div class="text-right">
+            <p class="text-1">QUY TRÌNH PHẪU THUẬT</p>
+            <span class="line"></span>
+            <p class="text-2">Thời gian phẫu thuật khá nhanh, thường kéo dài trong 10-15 phút</br></br>
+                    <b>Bước 1: Gây tê</b></br>
+                    Người bệnh sẽ được gây tê ở gốc dương vật, đảm bảo gây tê toàn bộ dương vật.</br></br>
+                    <b>Bước 2: Tiêm Filler vào viền quy đầu</b></br>
+                    Bác sĩ sẽ dùng những loại kim nhỏ để đưa Filler vào viền quy đầu. Kim càng nhỏ thì nguy cơ chảy
+                     máu càng ít. Lượng Filler sẽ được đưa đều vào phần viền của quy đầu, tạo cho viền quy đầu to đều,
+                      không bị gồ ghề.</br></br>
+                    <b>Bước 3: Băng gạc và sát trùng</b></br>
+                    Tiêm Filler xong thì bác sĩ sẽ bịt chặt chỗ tiêm để bịt kín lỗ kim khi chọc vào viền quy đầu. Sau đó
+                     bạn sẽ được băng bằng gạc và sát trùng bằng Betadin.</p>
+        </div>
+    </div>
+    <div class="ss7">
+        <div class="img">
+            <p class="text-1">Thư viện</p>
+            <p class="text-2">khám phá</p>
+            <span class="line"></span>
+        </div>
+    </div>
+    <div class="ss8">
+        <div class="text-left">
+            <p class="text-1">KHI NÀO BẠN NÊN THỰC HIỆN PHẪU THUẬT LÀM TO VIỀN QUY ĐẦU</p>
+            <span class="line"></span>
+            <p class="text-2">The general technique used during penile reconstruction is called radial artery-based
+                forearm phalloplasty, or RAP. Within your forearm, there are two large arteries which supply the forearm
+                with blood. During RAP, one of the arteries and at least one large nerve as well as external skin and
+                the soft tissue which protects the bone is removed and rolled into a tube. Dr. Elist will attach
+                this neophallus to the blood and nerve tissues in the groin. You may also choose to have a penile implant
+                installed at this time in order to achieve an erection.</p>
+            <button class="btn-contact"><p class="text-3">LIÊN HỆ CHUYÊN GIA</p></button>
+
+        </div>
+        <div class="text-right"></div>
+    </div>
+</div>
+
+@endsection
