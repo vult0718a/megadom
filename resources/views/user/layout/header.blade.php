fc21e0fa7e8b8a5f3d79a23e8ccf06993ab85bd0
vult0718a <hoangvu3600@gmail.com>
2022-11-17 23:59:32 +0700

    respon manu

--- resources/views/user/layout/header.blade.php
+++ resources/views/user/layout/header.blade.php
@@ -1,6 +1,6 @@
-<nav class="header">
+<!-- <nav class="header">
   <a href="{{ route('home') }}"><img id="logo" src="/frontend/images/logo-megadom-1.png"></a>
-  <img id="icon" src="/frontend/images/phone.png"><span class="hotline">Hotline</span>
+  
   <p>098 534 2323</p>
   <div><span>ĐẶT LỊCH KHÁM</span></div>
   <hr>
@@ -11,4 +11,48 @@
   <a id="item-4" href="{{ route('library') }}">THƯ VIỆN</a>
   <a id="item-5" href="{{ route('post') }}">TIN TỨC</a>
   <a id="item-6" href="{{ route('lien_he') }}">LIÊN HỆ</a>
+</nav> -->
+
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="topbar-logo">
+        <div>
+            <div class="row" style="padding-top: 22px;">
+                <div class="col-xs-6">
+                  <a href="{{ route('home') }}"><img id="logo" src="/frontend/images/logo-megadom-1.png"></a>
+                </div>
+                <div class="col-xs-6 navbar-right text-right">
+                    <div>
+                    <img id="icon" src="/frontend/images/phone.png"><span class="hotline">Hotline</span>
+                        <p>098 534 2323</p>
+                    </div>
+                    <div>
+                        <span class="text-topbar">ĐẶT LỊCH KHÁM</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container" style="width: 90%;">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <div class="toggle-products"><i class="fa fa-th-large" aria-hidden="true"></i>Danh mục</div>
+            </button>
+        </div>
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li><a href="#">VỀ MEGADOM</a></li>
+                        <li><a href="#">THẨM MỸ NAM KHOA</a></li>
+                        <li><a href="{{ route('erectile-dysfunction') }}">BỆNH LÝ NAM KHOA</a></li>
+                        <li><a href="{{ route('library') }}">THƯ VIỆN</a></li>
+                        <li><a href="{{ route('post') }}">TIN TỨC</a></li>
+                        <li><a href="{{ route('lien_he') }}">LIÊN HỆ</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 </nav>
--- resources/views/user/layout/main.blade.php
+++ resources/views/user/layout/main.blade.php
@@ -34,23 +34,38 @@
     @include('user.layout.header')
 
     <!-- Content -->
-    @yield('content')
+    <!-- @yield('content') -->
 
     <!-- footer -->
-    @include('user.layout.footer')
+    <!-- @include('user.layout.footer') -->
 
     <!-- js jquery bootstrap -->
-    <script type="text/javascript" src="/frontend/bootstrap/js/bootstrap.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     
     <!-- siick slider -->
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="/frontend/owlcarousel/owl.carousel.min.js"></script>
+    <script type="text/javascript" src="/frontend/bootstrap/js/bootstrap.min.js"></script>
     
     <!-- javascrip -->
     <script type="text/javascript" src="/frontend/css/javascrip.js"></script>
     <script src='https://cdnjs.cloudflare.com/ajax/libs/flickity/1.0.0/flickity.pkgd.js'></script>
+    <script>
+        $(document).ready(function () {
+            var mql = window.matchMedia("screen and (max-width: 767px)")
+            mediaqueryresponse(mql)
+            mql.addListener(mediaqueryresponse)
+
+            function mediaqueryresponse(mql) {
+                if (mql.matches) {
+                    $('#menu-top').removeClass('in');
+                } else {
+                    $('#menu-top').addClass('in');
+                }
+            }
+        });
+    </script>
 
     <!-- Import script -->
     @yield('script')
