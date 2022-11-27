ae83aa29a1baf6f14158bbdb418d7ef163065010
vult0718a <hoangvu3600@gmail.com>
2022-11-28 00:49:18 +0700

    feat: screen 1.2

--- resources/views/user/page/doctor.blade.php
+++ resources/views/user/page/doctor.blade.php
@@ -9,132 +9,216 @@
 @endsection
 
 @section('content')
-  <div class="doctors">
-    <div class="banner">
-      <div class="text-banner">
-        <p class="text-small">TRANG CHỦ. VỀ MEGADOM. ĐỘI NGŨ BÁC SĨ</p>
-        <p class="text-bold">Đội ngũ bác sĩ</p>
+  <div class="body-content">
+    <div class='main-wrapper'>
+      <div class="banner-container">
+        <div class="banner-content">
+          <p class="banner-text-small">TRANG CHỦ. VỀ MEGADOM. ĐỘI NGŨ BÁC SĨ</p>
+          <p class="banner-text-big">Đội ngũ bác sĩ</p>
+        </div>
       </div>
     </div>
-    <div class="doctor-info">
-      <p class="text-small">
-        <span>Ths. Bs Nguyễn Văn Đức tốt nghiệp chuyên ngành Bác sĩ Đa khoa trường Đại học Y Hà Nội. Từ năm 2015 đến nay công tác tại tại khoa phẫu thuật tiết niệu – Bệnh viện Xanh Pôn.</span>
-        <span>Ths. Bs Nguyễn Văn Đức hiện đang là thành viên của hội Nam khoa thế giới (ISSM ) và Hội thận học – Niệu khoa Việt Nam (VUNA).</span>
-        <span>Trên phạm vi toàn quốc, Thạc sĩ Nguyễn Văn Đức là bác sĩ đầu tiên đưa tấm độn sinh học Megadom® về Việt Nam và đã được Sở Y Tế cấp phép..</span>
-      </p>
-      <img id="image-1" src="/frontend/images/image14a.png">
-      <div class="learning">
-        <p class="text-bold">QUÁ TRÌNH HỌC TẬP VÀ CÔNG TÁC</p>
-        <hr>
-        <ul class="text-small">
-          <li>Trình độ học vấn: Thạc sĩ, Bác sĩ</li>
-          <li>Tốt nghiệp Đại học Y Hà Nội – chuyên ngành Bác sĩ đa khoa</li>
-          <li>Tốt nghiệp khoá đào tạo từ xa của Trường đại học Johns Hopkins – Mỹ năm 2017.</li>
-          <li>Hoàn thành khoá học “Phẫu thuật thẩm mỹ Dương vật” chuỗi hệ thống Phòng khám Nam khoa StanTop HD – Hàn Quốc.</li>
-          <li>Hoàn thành khoá học “Nam khoa chuyên sâu’ của hội PRISM (Post Graduate International School of Mens Health)</li>
-        </ul>
-      </div>
-      <div class="qualification">
-        <p class="text-bold">TRÌNH ĐỘ CHUYÊN MÔN</p>
-        <hr>
-        <ul class="text-small">
-          <li>Thành viên của Hội Y học giới tính thế giới (ISSM)</li>
-          <li>Thành viên của hội Tiết Niệu – Thận học Việt Nam.</li>
-          <li>Tham gia hàng trăm ca phẫu thuật thẩm mỹ dương vật tại Hàn Quốc.</li>
-          <li>Gần 10 năm kinh nghiệm điều trị và phẫu thuật tại Khoa Phẫu thuật Tiết Niệu – Nam Học, Bệnh viện đa khoa Xanh Pôn.</li>
-        </ul>
-      </div>
-    </div>
-    <div class="degree">
-      <div class="title">
-        <p class="text-bold">BẰNG CẤP VÀ CHỨNG CHỈ QUỐC TẾ</p>
-        <hr>
-      </div>
-      <div class="slider">
-        <div class="gallery js-flickity"
-          data-flickity-options='{ "wrapAround": true }'>
-          <div class="gallery-cell">
-            <img src="/frontend/images/rectangle116.png" width="100%" />
+
+    <div class='main-content'>
+      <div class="row">
+        <div class="col-xl-6 col-lg-6 col-sm-12 col-12 main-content-text">
+          <div style="width: 95%;" class="float-left">
+            <span>Ths. Bs Nguyễn Văn Đức tốt nghiệp chuyên ngành Bác sĩ Đa khoa trường Đại học Y Hà Nội. Từ năm 2015 đến nay công tác tại tại khoa phẫu thuật tiết niệu – Bệnh viện Xanh Pôn. </span><br>
+            <br><br><span>Ths. Bs Nguyễn Văn Đức hiện đang là thành viên của hội Nam khoa thế giới (ISSM ) và Hội thận học – Niệu khoa Việt Nam (VUNA). </span><br>
+            <br><br><span>Trên phạm vi toàn quốc, Thạc sĩ Nguyễn Văn Đức là bác sĩ đầu tiên đưa tấm độn sinh học Megadom® về Việt Nam và đã được Sở Y Tế cấp phép..</span><br>
           </div>
-          <div class="gallery-cell">
-            <img src="/frontend/images/rectangle116.png" width="100%" />
-          </div>
-          <div class="gallery-cell">
-            <img src="/frontend/images/rectangle116.png" width="100%" />
-          </div>
-          <div class="gallery-cell">
-            <img src="/frontend/images/rectangle116.png" width="100%" />
-          </div>
-          <div class="gallery-cell">
-            <img src="/frontend/images/rectangle116.png" width="100%" />
+        </div>
+        <div class="col-xl-6 col-lg-6 col-sm-12 col-12">
+          <div style="width: 95%;" class="float-right">
+            <img src="/frontend/images/image1-2-1.png">
           </div>
         </div>
       </div>
     </div>
-    <div class="booking">
-      <p class="booking-title">ĐẶT LỊCH TƯ VẤN MIỄN PHÍ</p>
-      <div class="block-booking"><span>ĐĂNG KÝ NGAY</span></div>
+
+    <div class='main-content'>
+      <div class="row">
+        <div class="col-xl-6 col-lg-6 col-sm-12 col-12 main-content-text">
+          <div style="width: 95%;" class="float-left">
+            <h3 class="title-header-content">QUÁ TRÌNH HỌC TẬP VÀ CÔNG TÁC</h3>
+            <hr class="line-header-content">
+            <div style="margin-top: 70px">
+              <ul class="content-text-1">
+                <li>Trình độ học vấn: Thạc sĩ, Bác sĩ</li>
+                <li>Tốt nghiệp Đại học Y Hà Nội – chuyên ngành Bác sĩ đa khoa</li>
+                <li>Tốt nghiệp khoá đào tạo từ xa của Trường đại học Johns Hopkins – Mỹ năm 2017.</li>
+                <li>Hoàn thành khoá học “Phẫu thuật thẩm mỹ Dương vật” chuỗi hệ thống Phòng khám Nam khoa StanTop HD – Hàn Quốc.</li>
+                <li>Hoàn thành khoá học “Nam khoa chuyên sâu’ của hội PRISM (Post Graduate International School of Mens Health)</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xl-6 col-lg-6 col-sm-12 col-12">
+          <div style="width: 95%;" class="float-right">
+            <h3 class="title-header-content">TRÌNH ĐỘ CHUYÊN MÔN</h3>
+            <hr class="line-header-content">
+            <div style="margin-top: 70px">
+              <ul class="content-text-1">
+                <li>Thành viên của Hội Y học giới tính thế giới (ISSM)</li>
+                <li>Thành viên của hội Tiết Niệu – Thận học Việt Nam.</li>
+                <li>Tham gia hàng trăm ca phẫu thuật thẩm mỹ dương vật tại Hàn Quốc.</li>
+                <li>Gần 10 năm kinh nghiệm điều trị và phẫu thuật tại Khoa Phẫu thuật Tiết Niệu – Nam Học, Bệnh viện đa khoa Xanh Pôn.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="qa">
-      <div class="qa-title">
-        <p class="text-bold">HỎI ĐÁP CHUYÊN GIA</p>
-        <hr>
-      </div>
-      <div class="block block-1">
-        <img id="image-2" src="/frontend/images/rectangle81a.png">
-        <p class="text-bold">CHUỖI HẠT NGỌ KHÁC SÙI MÀO GÀ NHƯ THẾ NÀO? CÓ NGUY HIỂM KHÔNG?</p>
-      </div>
-      <div class="block block-2">
-        <img id="image-3" src="/frontend/images/rectangle81b.png">
-        <p class="text-bold">NHỮNG DẠNG DA BAO QUY ĐẦU CẦN CẮT</p>
-      </div>
-      <div class="block block-3">
-        <img id="image-4" src="/frontend/images/rectangle81c.png">
-        <p class="text-bold">SỰ THẬT VỀ THUỐC TRÁNH THAI DÀNH CHO NAM</p>
+
+    <div class='main-content-full'>
+      <div >
+          <h3 class="title-body-content-full">BẰNG CẤP VÀ CHỨNG CHỈ QUỐC TẾ</h3>
+          <hr class="line-header-content-center">
+          <div class="slider">
+          <div class="gallery js-flickity" data-flickity-options='{ "wrapAround": true }'>
+            <div class="gallery-cell">
+              <img src="/frontend/images/rectangle116.png" width="100%" />
+            </div>
+            <div class="gallery-cell">
+              <img src="/frontend/images/rectangle116.png" width="100%" />
+            </div>
+            <div class="gallery-cell">
+              <img src="/frontend/images/rectangle116.png" width="100%" />
+            </div>
+            <div class="gallery-cell">
+              <img src="/frontend/images/rectangle116.png" width="100%" />
+            </div>
+            <div class="gallery-cell">
+              <img src="/frontend/images/rectangle116.png" width="100%" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-    <div class="surgery-list">
-      <div class="surgery-title">
-        <p class="text-bold">HỎI ĐÁP CHUYÊN GIA</p>
-        <hr>
+
+    <div class='main-wrapper-2'>
+      <div class="banner-container-2">
+        <div class="banner-content-2">
+          <p class="banner-text-big-2">ĐẶT LỊCH TƯ VẤN MIỄN PHÍ</p>
+          <button href="#" class="button-QA">
+              <span>ĐĂNG KÝ NGAY</span>
+          </button>
+        </div>
       </div>
-      <p class="text-small">
-        <span>Khi thuốc, thực phẩm chức năng hoặc các phương pháp tự nhiên không hiệu quả trong việc giải quyết các vấn đề sức khỏe của bệnh nhân, thì phẫu thuật có thể là lựa chọn thích hợp.</span>
-        <span>ThS Nguyễn Văn Đức, bác sĩ tiết niệu và phẫu thuật nam khoa với gần 10 năm kinh nghiệm, chuyên về tiết niệu, sức khỏe tình dục nam giới, tăng cường sinh lý nam và khả năng sinh sản cũng như các phẫu thuật khác nhau để giải quyết vô số vấn đề mà nam giới có thể gặp phải, bao gồm rối loạn cương dương, xuất tinh sớm hoặc kích thước dương vật không đạt yêu cầu.</span>
-      </p>
-      <p class="text-medium">DANH MỤC PHẪU THUẬT</p>
-      <ul class="text-ul">
-        <li>Phẫu thuật dương vật: <a>Phẫu thuật tăng kích thước dương vật bằng tấm độn sinh học</a>, <a>Phẫu thuật tăng kích thước dương vật bằng silicon</a>, <a>Phẫu thuật kéo dài dương vật</a>, Phẫu thuật thẩm mỹ cong dương vật</a>, Phẫu thuật thẩm mỹ chỉnh cong dương vật.Phẫu thuật điều trị vùi lấp dương vật</li>
-        <li>Phẫu thuật quy đầu: <a>Phẫu thuật cắt bao quy đầu</a>, <a>Phẫu thuật làm to quy đầu bằng Megafill</a>, <a>Phẫu thuật làm to quy đầu bằng Filler</a>.</li>
-        <li>Phẫu thuật khác: Phẫu thuật điều trị xuất tinh sớm, Tiểu phẫu gắn bi dương vật, Phẫu thuật cấp cứu gãy dương vật, Phẫu thuật điều trị ung thư dương vật.</li>
-      </ul>
     </div>
-    <div class="library">
-      <p class="text-bold">Thư viện</p>
-      <p class="text-small">khám phá</p>
-      <hr>
+
+    <div class='main-content'>
+      <div class="row">
+        <div class="col-xl-12 col-lg-12 col-sm-12 col-12 main-content-text">
+          <div>
+            <h3 class="title-header-content">HỎI ĐÁP CHUYÊN GIA</h3>
+            <hr class="line-header-content">
+            <div style="margin-top: 50px">
+              <div class="row">
+                <div class="col-xl-4 col-lg-4 col-sm-12 col-12 main-content-image">
+                  <div>
+                    <img src="/frontend/images/image1-2-4.png">
+                    <p>CHUỖI HẠT NGỌ KHÁC SÙI MÀO GÀ NHƯ THẾ NÀO? CÓ NGUY HIỂM KHÔNG?</p>
+                  </div>
+                </div>
+
+                <div class="col-xl-4 col-lg-4 col-sm-12 col-12 main-content-image">
+                  <div>
+                    <img src="/frontend/images/image1-2-5.png">
+                    <p>NHỮNG DẠNG DA BAO QUY ĐẦU CẦN CẮT</p>
+                  </div>
+                </div>
+
+                <div class="col-xl-4 col-lg-4 col-sm-12 col-12 main-content-image">
+                  <div>
+                    <img src="/frontend/images/image1-2-6.png">
+                    <p>SỰ THẬT VỀ THUỐC TRÁNH THAI DÀNH CHO NAM</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="feeling">
-      <div class="feeling-title">
-        <p class="text-bold">CẢM NHẬN KHÁCH HÀNG</p>
-        <hr>
+
+    <div class='main-content'>
+      <div class="row">
+        <div class="col-xl-12 col-lg-12 col-sm-12 col-12 main-content-text">
+          <div>
+            <h3 class="title-header-content">PHẪU THUẬT NAM KHOA</h3>
+            <div>
+              <div class="row">
+                <div class="col-xl-6 col-lg-6 col-sm-12 col-12 main-content-text">
+                  <div style="width: 95%;" class="float-left">
+                    <hr class="line-header-content">
+                    <div style="margin-top: 60px">
+                      <span>Khi thuốc, hực phẩm chức năng hoặc các phương pháp tự nhiên không hiệu quả trong việc giải quyết các vấn đề sức khỏe của bệnh nhân, thì phẫu thuật có thể là lựa chọn thích hợp.</span>
+                      <br><br><br><span>ThS Nguyễn Văn Đức, bác sĩ tiết niệu và phẫu thuật nam khoa với gần 10 năm kinh nghiệm, chuyên về tiết niệu, sức khỏe tình dục nam giới, tăng cường sinh lý nam và khả năng sinh sản cũng như các phẫu thuật khác nhau để giải quyết vô số vấn đề mà nam giới có thể gặp phải, bao gồm rối loạn cương dương, xuất tinh sớm hoặc kích thước dương vật không đạt yêu cầu.</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-xl-6 col-lg-6 col-sm-12 col-12">
+                  <div style="width: 95%;" class="float-right">
+                    <span class="text-title-content-4">DANH MỤC PHẪU THUẬT</span>
+                    <ul class="content-text-1" style="margin-top: 40px">
+                      <li>Phẫu thuật dương vật: <span class="text-under-line">Phẫu thuật tăng kích thước dương vật bằng tấm độn sinh học</span>, <span class="text-under-line">Phẫu thuật tăng kích thước dương vật bằng silicon</span>, <span class="text-under-line">Phẫu thuật kéo dài dương vật</span>, Phẫu thuật thẩm mỹ cong dương vật, Phẫu thuật thẩm mỹ chỉnh cong dương vật.Phẫu thuật điều trị vùi lấp dương vật</li>
+                      <li>Phẫu thuật quy đầu: <span class="text-under-line">Phẫu thuật cắt bao quy đầu</span>, <span class="text-under-line">Phẫu thuật làm to quy đầu bằng Megafill</span>, <span class="text-under-line">Phẫu thuật làm to quy đầu bằng Filler</span>.</li>
+                      <li>Phẫu thuật khác: Phẫu thuật điều trị xuất tinh sớm, Tiểu phẫu gắn bi dương vật, Phẫu thuật cấp cứu gãy dương vật, Phẫu thuật điều trị ung thư dương vật.</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="block-feeling feeling-1">
-        <p class="text-small">“Trước kia tôi còn ngần ngại khi nghĩ đến việc làm to "chỗ ấy". Nhưng để cải thiện chuyện chăn gối, tôi đã tìm hiểu từ nhiều nguồn thông tin.  Thật sự đã không lầm khi đặt niềm tin vào Megadom. Tôi đã tự tin hơn rất nhiều!”</p>
-        <p class="text-medium">_ Mr. Cong</p>
-        <hr>
-      </div>
-      <div class="block-feeling feeling-2">
-        <p class="text-small">“Chỉ khi gặp Megadom, tôi mới thấy nhẹ người vì được "bắt đúng bênh". Không hề đau đơn hay khó khăn gì như tôi lo sợ, giờ thì vợ tôi còn thích hơn cả..”</p>
-        <p class="text-medium">_ Mr. Cong</p>
-        <hr>
-      </div>
-      <div class="block-feeling feeling-3">
-        <p class="text-small">“Bác sĩ nhiệt tình và chu đáo nên tôi cảm thấy an tâm. Phẫu thuật cũng không đau đớn gì và hiệu quả rõ rệt. Tôi nghĩ mình đã chọn đúng!”</p>
-        <p class="text-medium">_ Mr. Cong</p>
-        <hr>
-      </div>
-      <div class="see-all"><span>XEM TẤT CẢ</span></div>
     </div>
+
+    <div class='main-wrapper-3'>
+      <div class="banner-container-3">
+        <div class="banner-content-3">
+          <p class="banner-text-big-3">Thư viện</p>
+          <p class="banner-text-under-3">khám phá</p>
+          <hr class="under-3">
+        </div>
+      </div>
+    </div>
+
+
+    <div class='main-content' style="margin-bottom: 142px;">
+      <div class="row">
+        <div class="col-xl-12 col-lg-12 col-sm-12 col-12 main-content-text">
+          <div>
+            <h3 class="title-header-content">CẢM NHẬN KHÁCH HÀNG</h3>
+            <hr class="line-header-content">
+            <div style="margin-top: 70px;">
+              <div style="margin-top: 36px;">
+                <p>“Trước kia tôi còn ngần ngại khi nghĩ đến việc làm to "chỗ ấy". Nhưng để cải thiện chuyện chăn gối, tôi đã tìm hiểu từ nhiều nguồn thông tin.  Thật sự đã không lầm khi đặt niềm tin vào Megadom. Tôi đã tự tin hơn rất nhiều!”</p>
+                <p style="margin: 44px 37px;"><b>_ Mr. Cong</b></p>
+                <hr>
+              </div>
+
+              <div style="margin-top: 36px;">
+                <p>“Chỉ khi gặp Megadom, tôi mới thấy nhẹ người vì được "bắt đúng bênh". Không hề đau đơn hay khó khăn gì như tôi lo sợ, giờ thì vợ tôi còn thích hơn cả..”</p>
+                <p style="margin: 44px 37px;"><b>_ Mr. Cong</b></p>
+                <hr>
+              </div>
+
+              <div style="margin-top: 36px;">
+                <p>“Bác sĩ nhiệt tình và chu đáo nên tôi cảm thấy an tâm. Phẫu thuật cũng không đau đớn gì và hiệu quả rõ rệt. Tôi nghĩ mình đã chọn đúng!”</p>
+                <p style="margin: 44px 37px;"><b>_ Mr. Cong</b></p>
+                <hr>
+              </div>
+            </div>
+          </div>
+          <button href="#" class="button-QA">
+              <span>XEM TẤT CẢ</span>
+          </button>
+        </div>
+      </div>
+    </div>
+
   </div>
 @endsection
