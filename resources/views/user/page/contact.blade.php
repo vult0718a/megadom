f12455b7a6d92774509fbacd3bf65f8a200fbec6
datdt <datdt@hblab.vn>
2022-12-03 19:32:46 +0700

    fix: contact screen

--- resources/views/user/page/contact.blade.php
+++ resources/views/user/page/contact.blade.php
@@ -13,15 +13,15 @@
   <div class="contact-screen">
     <div class="banner">
       <div class="text-banner">
-        <p class="text-small">TRANG CHỦ. LIÊN HỆ</p>
+        <p class="text-medium">TRANG CHỦ. LIÊN HỆ</p>
         <p class="text-bold">Liên hệ</p>
       </div>
     </div>
     <div class="form-input">
-      <p class="text-normal">PHÒNG KHÁM NAM KHOA MEGADOM</p>
-      <div class="contact contact-1"><img id="icon" src="/frontend/images/phone.png"><span>098 534 2323</span></div>
-      <div class="contact contact-2"><img id="icon" src="/frontend/images/vector1a.png"><span>megaderm.vn@gmail.com</span></div>
-      <div class="contact contact-3"><img id="icon" src="/frontend/images/vector1b.png"><span>Tầng 2, Bệnh viện Đức Phúc- Số 48 P. Ô Đồng Lâm, Phương Liên, Đống Đa, Hà Nội</span></div>
+      <p class="name">PHÒNG KHÁM NAM KHOA MEGADOM</p>
+      <div class="contact contact-1 text-medium"><img id="icon" src="/frontend/images/phone.png"><span>098 534 2323</span></div>
+      <div class="contact contact-2 text-medium"><img id="icon" src="/frontend/images/vector1a.png"><span>megaderm.vn@gmail.com</span></div>
+      <div class="contact contact-3 text-medium"><img id="icon" src="/frontend/images/vector1b.png"><span>Tầng 2, Bệnh viện Đức Phúc- Số 48 P. Ô Đồng Lâm, Phương Liên, Đống Đa, Hà Nội</span></div>
       <form>
         <div class="input-block">
           <div class="block block-1">
@@ -56,9 +56,9 @@
               <option value="Google">Google</option>
             </select>
           </div>
-          <div class="rule-title">Điều khoản dịch vụ<span>*</span></div>
+          <div class="rule-title text-small">Điều khoản dịch vụ<span>*</span></div>
           <div class="rules">
-            <div class="rules-content">By submitting this form I give Megadom, a Medical Corporation, Penuma, and its affiliates permission to contact me and agree to the terms of the <span>Privacy Policy</span>.</div>
+            <textarea class="rules-content">By submitting this form I give Megadom, a Medical Corporation, Penuma, and its affiliates permission to contact me and agree to the terms of the <span>Privacy Policy</span>.</textarea>
             <div class="accept"><input type="checkbox" require name="accept" id="accept"><span>I accept the Terms and Conditions.</span></div>
             <button type="button" id="register"><span class="register">ĐĂNG KÝ TƯ VẤN</span></button>
           </div>
@@ -66,7 +66,6 @@
       </form>
       </div>
     </div>
-    <div class="block-hide"></div>
   </div>
 @endsection
 
